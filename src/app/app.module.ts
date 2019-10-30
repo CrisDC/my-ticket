d6552fd10d20976266cae33b9a36eb0882ec753d
CrisDC <cristhianw11@hotmail.com>
2019-10-30 01:39:16 -0500

    modularización de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { BodyComponent } from './shared/components/body/body.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
